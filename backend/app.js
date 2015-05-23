28389bd71186d11fae0728947724b56a9081969a
Davis Robertson <epicdavibrickfilms@gmail.com>
2015-05-23 15:12:09 -0500

    Updating socket.io stuff

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,35 @@
 var app = require('http');
 var io = require('socket.io')(5000);
 
+// id -> json
+var users = {};
+
+function geIDrFromName(name){
+    var keys = Object.keys(users);
+    for(var i = 0; i<keys.length; i++){
+        var cuser = users[keys[i]];
+        //console.log(cuser);
+        if(cuser["name"]==name){
+            return keys[i];
+        }
+    }
+    return null;
+}
 
 io.on("connection", function(socket){
     console.log(socket.id);
+    socket.on("connect name", function (name) {
+        if(geIDrFromName(name)==null){
+            users[socket.id]= {name:name};
+        }else{
+            socket.emit("name taken");
+        }
+    });
+    socket.on("disconnect", function(){
+        console.log("dis "+socket.id);
+        if(users[socket.id]!=null){
+            delete users[socket.id];
+        }
+    });
 });
+
--- frontend/socketstuff.js
+++ frontend/socketstuff.js
@@ -1,12 +1,30 @@
 var socket = io('http://localhost:5000');
 
-$(document).ready(function(){
 
-});
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
+var name = "";
 
 socket.on('connect', function () {
-        console.log("dank");
-    }
-);
+    console.log("dank");
+});
 
+socket.on("name taken", function(){
+    $("body").append("<h1>That name has already been taken</h1>" +
+    "<h2>Go back <a href='index.html'>home</a>.</h2>");
+});
+
+$(document).ready(function(){
+    name = getParameterByName("name");
+    console.log(name);
+    if(name.length==0){
+        //window.location="index.html";
+    }
+    $("body").append("<h1>"+name+"</h1>");
+    socket.emit("connect name", name);
+});
