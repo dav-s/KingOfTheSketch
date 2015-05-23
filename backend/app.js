55b4a2246f234c7d589411e0bc9c8abe79fe4cea
Davis Robertson <epicdavibrickfilms@gmail.com>
2015-05-23 18:01:02 -0500

    Game socket updating

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 var io = require('socket.io')(5000);
 // id -> json
+
+var seconds_to_wait = 5;
 var users = {};
+
+
+var kingvotes= 0, peasvotes= 0;
+var kingid, peasid;
+
+var starttime;
+var gamegoing=false;
 
 function geIDrFromName(name){
     var keys = Object.keys(users);
@@ -30,5 +39,62 @@ io.on("connection", function(socket){
             delete users[socket.id];
         }
     });
+    socket.on("vote king", function(){
+        if(gamegoing) {
+            if (waitedTime(socket.id)){
+                users[socket.id]["lastvotetime"] = Date.now();
+                kingvotes++;
+                updateUI();
+            }else{
+                socket.emit("need wait", Date.now()-users[socket.id]["lastvotetime"]);
+            }
+        }
+    });
+    socket.on("vote peas", function(){
+        if(gamegoing) {
+            if (waitedTime(socket.id)){
+                users[socket.id]["lastvotetime"] = Date.now();
+                peasvotes++;
+                updateUI();
+            }else{
+                socket.emit("need wait", Date.now()-users[socket.id]["lastvotetime"]);
+            }
+        }
+    });
+    socket.on("start game", function () {
+        if(!gamegoing){
+            gamegoing=true;
+            starttime=Date.now();
+            kingvotes=0;
+            peasvotes=0;
+            updateUI();
+        }
+    });
+
 });
+
+function waitedTime(sid){
+    var lastvotetime = users[sid]["lastvotetime"];
+    var curtime = Date.now();
+    if(lastvotetime==null){
+        return true;
+    }
+    if((curtime-lastvotetime)/1000<=seconds_to_wait){
+        return true;
+    }
+
+    return false;
+}
+
+function updateUI(){
+    io.emit("update ui", getGameState());
+}
+
+function getGameState(){
+    return {
+        "peasvotes": peasvotes,
+        "kingvotes": kingvotes,
+        "time": starttime
+    };
+}
 
