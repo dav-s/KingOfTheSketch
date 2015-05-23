9dfa5c0b962f378b9f83566ba156bdad64b473ec
Ryan Wells <rawells14@gmail.com>
2015-05-23 16:44:12 -0500

    Changed UI positioning a bit

--- frontend/drawapp.js
+++ frontend/drawapp.js
@@ -1,10 +1,13 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     var p1 = new DrawingBoard.Board('player1',{
 		controls: [
-		'Color',
-		'Size'
+		'Size',
+		'Color'
+		
 	],
+	controlsPosition: 'top right'
     });
+
     var p2 = new DrawingBoard.Board('player2',{
 		controls: [
 		'Color',
